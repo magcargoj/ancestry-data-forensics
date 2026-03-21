8ba3fc812c1aa9368ce74edee2d017686e0968dc
magcargoj <magcargoj@github.com>
2026-03-21 14:47:20 -0400

    Fix build configuration paths

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,5 +1,5 @@
-import { QuartzConfig, setup } from "./quartz/util/ctx"
-import * as Plugin from "./quartz/plugins"
+import { QuartzConfig } from "./util/ctx"
+import * as Plugin from "./plugins"
 
 /**
  * Quartz 4.0 Configuration
